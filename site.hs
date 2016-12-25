a7cf19ff053bcad68850b41604d835770137391e
Tristan Sloughter <t@crashfast.com>
2016-12-25 14:58:31 -0800

    add rss feed

--- site.hs
+++ site.hs
@@ -47,6 +47,7 @@ main = hakyll $ do
 
           pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    indexCtx
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/default.html" indexCtx
             >>= relativizeUrls
 
@@ -91,10 +92,28 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" aboutCtx
                 >>= relativizeUrls
 
+    -- keep it the same path as the original howistart.org
+    create ["static/posts.rss"] $ do
+      route idRoute
+      compile $ do
+        let feedCtx = postCtx `mappend` bodyField "description"
+        posts <- fmap (take 10) . recentFirst =<<
+            loadAllSnapshots "posts/*/*/*.md" "content"
+        renderAtom myFeedConfiguration feedCtx posts
+
     match "templates/*" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
+
+myFeedConfiguration :: FeedConfiguration
+myFeedConfiguration = FeedConfiguration
+    { feedTitle       = "How I Start."
+    , feedDescription = "How I Start is a mix between a collection of development tutorials and The Setup."
+    , feedAuthorName  = "Tristan Sloughter"
+    , feedAuthorEmail = "contact@howistart.org"
+    , feedRoot        = "http://www.howistart.org"
+    }
 
 postCtx :: Context String
 postCtx = mconcat
